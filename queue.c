2780c6e3554e992359471765c27d5c8991ccf368
2gury <54157924+2gury@users.noreply.github.com>
2021-02-08 16:52:57 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -14,3 +14,21 @@ int change_process_state_in_queue(int pid, int changeState);
 int get_first_process_in_queue(void);
 /*Функция для удаления всех завершенных процессов из очереди процессов*/
 int remove_terminated_processes_from_queue(void);
+
+static struct semaphore mutex;
+
+sema_init(&mutex,1); 	
+
+void function(void) {
+
+    /*Проверка доступности семафора (если не доступен, то процесс будет помещен в очередь ожидания семафора). Блокирует семафор для осуществления определенных действий. Обеспечивает безопасный доступ к критической секции*/
+	if(down_interruptible(&mutex)){
+		printk(KERN_ALERT "Mutex error");
+		return -ERESTARTSYS;
+	}	
+
+  ...
+	
+   /*Освобождение критической секции для других процессов/потоков*/
+	up(&mutex);
+}
